a0aa542246ecf19c0d97c1376e18208db1fb1b0a
GRILLO-Benjamin <benjisensei@gmail.com>
2022-09-09 10:13:48 +0200

    firebase redirect with google auth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,45 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth'
+
+import {
+        // auth,
+        signInWithGooglePopup, 
+        // signInWithGoogleRedirect, 
+        createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils'
 
 
 const SignIn = () =>{
+    // useEffect(() =>{//callback
+    //     async function fetchData() {
+    //         const response = await getRedirectResult(auth);
+
+    //         if(response){
+    //             const userDocRef = await createUserDocumentFromAuth(response.user)
+    //         }
+    // }
+    // fetchData();
+    // }, [])//run once when called
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user)
     }
+
+    // const logGoogleRedirectUser = async () => {
+    //     const {user} = await signInWithGoogleRedirect();
+    //     console.log(user)
+    //     }
+
     return(
         <div>
             <h1>Sign in Page</h1>
             <button onClick={logGoogleUser} >
                 Sign in with Google Popup
             </button>
+            {/* <button onClick={signInWithGoogleRedirect} >
+                Sign in with Google Redirect
+            </button> */}
         </div>
     )
 }
